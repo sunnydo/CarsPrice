b6a56b0cb8fe6a85a0e1ece726ae215f5b512fe1
sunnydo <sunnydo@sunnys-imac.local>
2016-10-06 15:04:36 +0800

    Initial Commit

---
+++ CarsPrice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarsPrice
+//
+//  Created by Sunny Do on 06/10/2016.
+//  Copyright © 2016 Sunny Do. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
